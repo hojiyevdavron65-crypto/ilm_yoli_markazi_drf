3623d0f90c3c7d73b996041c3d9c5b04552a0427
hojiyevdavron65-crypto <hojiyevdavron65@gmail.com>
2026-07-25 07:58:48 +0500

    fixed problems

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        # "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
